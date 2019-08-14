f096ae35265d3ff25932a67342a5deb79c26ae05
Leo <130.0x82@gmail.com>
2019-08-14 13:21:31 +0200

    feat: added Disclaimer Component

---
+++ src/components/Disclaimer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Disclaimer = (props) => {
+    return (
+        <div class="disclaimer">
+            <span class="small">
+                Made with ❤️by members of <a target="_blank" href="http://hej.digital">hej.digital</a>. 
+            </span>
+        </div>
+    )
+}
+
+export default Disclaimer
