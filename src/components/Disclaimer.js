0302ffd5c90d956865f83c31ef8b793a43841e83
leonat <130.0x82@gmail.com>
2020-08-10 12:53:49 +0200

    Update Disclaimer.js

--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -4,7 +4,7 @@ const Disclaimer = (props) => {
     return (
         <div class="disclaimer">
             <span class="small">
-                Made with ❤️  by members of <a target="_blank" href="http://hej.digital">hej.digital</a>. 
+                Made by Leonard Puhl, Jelko Arnds and Luca Iuzzolino &horbar; Moderated by students of <a target="_blank" href="http://digitalmedia-bremen.de">digital media</a> at hfk. 
             </span>
         </div>
     )
