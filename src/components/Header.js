ba7ece14447da817f3fef3897cd6583e1982e574
Roberta Jakob <rjakob@uni-bremen.de>
2023-07-21 13:41:23 +0200

    aktuelle links zu chat Gruppen als liste

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,8 +26,11 @@ const Header = (props) => {
                If you have any questions: Just ask us in the box below or on Discord/Whatsapp! The student hej-digital-team will update the missing pieces of information.
                <br /><br />
                These groups will connect you with other students and guide you through the orientation week: 
- <a target="blank" href="https://discord.gg/d9puneyYd" >Discord Server</a> 
-            <br></br>
+                <ul type="square">
+                    <li> <a target="blank" href="https://discord.gg/d9puneyYd" >Discord Server</a> </li>
+                    <li> <a target="blank" href="https://chat.whatsapp.com/CxgI8UypLP5IPA9p04HXoS" >Whatsapp Group for all Bachelor first semester students</a>  </li>
+                    <li> <a target="blank" href="https://chat.whatsapp.com/Ik7dlTvVHPf6LZT2lm9bTt" >Whatsapp Group for all Master first semester students </a>  </li>
+                </ul>
             <br></br>
                The orientation week of the entire Digital Media program will take place from the 09.10 - 13.10.2023
                 <br />
